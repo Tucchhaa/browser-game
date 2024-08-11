eb0aa983650f166d0ed468af16a83dfbd8bb4cb7
Eldar Iusupzhanov <ferrarijed@gmail.com>
2024-08-11 13:03:41 +0600

    rename files

---
+++ src/engine/core/engine-event-listener.ts
@@ -0,0 +1,35 @@
+export class EngineEventListener {
+    private static entities: EngineEventListener[] = [];
+
+    constructor() {
+        EngineEventListener.entities.push(this);
+    }
+
+    async setup() {}
+
+    async teardown() {}
+
+    beforeRender() {}
+
+    afterRender() {}
+
+    static async setup() {
+        for (const entity of EngineEventListener.entities)
+            await entity.setup();
+    }
+
+    static async teardown() {
+        for (const entity of EngineEventListener.entities)
+            await entity.teardown();
+    }
+
+    static beforeRender() {
+        for (const entity of EngineEventListener.entities)
+            entity.beforeRender();
+    }
+
+    static afterRender() {
+        for (const entity of EngineEventListener.entities)
+            entity.afterRender();
+    }
+}
